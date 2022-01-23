659a7a2b71657e153ff35edd8b4d5fb051317232
unknown <lf.passos11@gmail.com>
2022-01-22 21:00:13 -0300

    Update 22 jan 22

---
+++ payflow/lib/app_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'modules/login/login_page.dart';
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Pay Flow',
+        theme: ThemeData(
+            primaryColor: AppColors.primary, primarySwatch: Colors.orange),
+        home: LoginPage(),
+        initialRoute: "/splash",
+        routes: {
+          "/splash": (context) => SplashPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage()
+        });
+  }
+}
